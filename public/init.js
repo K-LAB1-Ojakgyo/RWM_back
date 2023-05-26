aaaed698844ae8c78e3f04e184f9cbee27c76c23
KwanHee Lee <kwan7595@naver.com>
2023-05-26 16:38:25 +0900

    api implementation  / test dummy code

---
+++ public/init.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkV8lUovkLmrAaK9Kj-vKHN7jpx0rjA8U",
+    authDomain: "k-lab-rwm.firebaseapp.com",
+    databaseURL: "https://k-lab-rwm-default-rtdb.firebaseio.com",
+    projectId: "k-lab-rwm",
+    storageBucket: "k-lab-rwm.appspot.com",
+    messagingSenderId: "575312651295",
+    appId: "1:575312651295:web:11a43bd9e4d0e4f65c212a",
+    measurementId: "G-XS7JCQSRQ9"
+  };
+  
+  // Initialize Firebase
+  //const firebase = firebase.initializeApp(firebaseConfig)
+
+  //export { firebase }
