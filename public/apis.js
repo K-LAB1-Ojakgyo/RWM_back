02e4526f62cc3d9207efcf62378d3d42f42dd53a
drrobot333 <30339175+drrobot333@users.noreply.github.com>
2023-05-31 20:44:15 +0900

    Update apis.js

--- public/apis.js
+++ public/apis.js
@@ -108,3 +108,34 @@ const updateBook = async (bookID, newData) => {
     throw error;
   }
 };
+
+const getAllBook = async () => {
+  const dbRef = firebase.database().ref("books");
+  try {
+    const snapshot = await dbRef.get();
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      console.log("No data available");
+      return null; // or any default value you prefer
+    }
+  } catch (error) {
+    console.error(error);
+    throw error; // rethrow the error to handle it outside the function
+  }
+};
+
+const getRandomBook = async (bookNum) => {
+  books = await getAllBook();
+  books_keys = Object.keys(books);
+  books_keys.sort(function () {
+    return Math.round(Math.random()) - 0.5;
+  });
+
+  book_list = {};
+  for (let i = 0; i < bookNum; i++) {
+    book_list[books_keys[i]] = books[books_keys[i]];
+  }
+
+  return book_list;
+};
